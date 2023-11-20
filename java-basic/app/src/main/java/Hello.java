25a8d253c5baf2ea3349e7f8e67f76daabf00169
dhkfkr134 <dhkfkr134@naver.com>
2023-11-20 17:47:39 +0900

    practice gradle build

--- java-basic/app/src/main/java/Hello.java
+++ java-basic/app/src/main/java/Hello.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Hello {
@@ -11,14 +12,32 @@ public class Hello {
     }
 
     public int getNum() {
-        int input = scanner.nextInt();
-        scanner.nextLine(); // 개행 문자 처리
+        int input;
+        while (true) {
+            try {
+                input = scanner.nextInt();
+                scanner.nextLine(); // 개행 문자 처리
+                break;
+            } catch (InputMismatchException e) {
+                System.out.println("Please enter a valid number:");
+                scanner.nextLine(); // 잘못된 입력을 처리하기 위해 버퍼를 비움
+            }
+        }
         return input;
     }
 
     public char getOper() {
-        char operation = scanner.next().charAt(0);
-        scanner.nextLine(); // 개행 문자 처리
+        char operation;
+        while (true) {
+            try {
+                operation = scanner.next().charAt(0);
+                scanner.nextLine(); // 개행 문자 처리
+                break;
+            } catch (StringIndexOutOfBoundsException e) {
+                System.out.println("Please enter a valid operator:");
+                scanner.nextLine(); // 잘못된 입력을 처리하기 위해 버퍼를 비움
+            }
+        }
         return operation;
     }
 
@@ -56,7 +75,7 @@ public class Hello {
     }
 
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        System.out.println("Hello, World 안녕 세계");
         Hello h1 = new Hello();
         h1.calculate();
         h1.scanner.close(); // 프로그램 종료 시 Scanner 닫기
