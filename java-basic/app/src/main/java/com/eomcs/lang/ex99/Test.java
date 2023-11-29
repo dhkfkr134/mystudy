d7247093cd8b62c8d1556bb723c7fc1c1c903aa8
dhkfkr134 <dhkfkr134@naver.com>
2023-11-29 18:37:26 +0900

    practice method&class filed

--- java-basic/app/src/main/java/com/eomcs/lang/ex99/Test.java
+++ java-basic/app/src/main/java/com/eomcs/lang/ex99/Test.java
@@ -3,11 +3,22 @@ package com.eomcs.lang.ex99;
 public class Test {
   public static void main(String[] args) {
 
-    float x = 5.0f;
-    float y = 0.0f;
-
-
-
-    System.out.println(Double.isInfinite(x % y));
+    java.util.Scanner scanner = new java.util.Scanner(System.in);
+    int all = Integer.parseInt(scanner.nextLine());
+    int num = Integer.parseInt(scanner.nextLine());
+    int sum = 0;
+    int price = 0;
+    int count = 0;
+    for (int i = 0; i < num; i++) {
+      price = scanner.nextInt();
+      count = scanner.nextInt();
+      scanner.nextLine();
+      sum += price * count;
+    }
+    scanner.close();
+    if (all == sum) {
+      System.out.print("yes");
+    } else
+      System.out.print("no");
   }
 }
