916d0f9cc2fe04f7d860ac3cb4ad3ca7da11200f
dhkfkr134 <dhkfkr134@naver.com>
2024-01-03 18:20:26 +0900

    32 : add [input/output] primitive & String type using Inheritance

--- myapp/app/src/main/java/bitcamp/io/DataOutputStream.java
+++ myapp/app/src/main/java/bitcamp/io/DataOutputStream.java
@@ -11,13 +11,21 @@ public class DataOutputStream extends FileOutputStream {
     super(name);
   }
 
+  public void writeBoolean(boolean value) throws IOException {
+    if (value) {
+      write(1);
+    } else {
+      write(0);
+    }
+  }
+
   public void writeUTF(String value) throws IOException {
     byte[] bytes = value.getBytes(StandardCharsets.UTF_8);
     writeShort(bytes.length);
     write(bytes);
   }
 
-  public void writeLong(long value) throws IOException{
+  public void writeLong(long value) throws IOException {
     write((int) (value >> 56));
     write((int) (value >> 48));
     write((int) (value >> 40));
@@ -28,7 +36,7 @@ public class DataOutputStream extends FileOutputStream {
     write((int) value);
   }
 
-  public void writeInt(int value) throws IOException{
+  public void writeInt(int value) throws IOException {
     write(value >> 24);
     write(value >> 16);
     write(value >> 8);
@@ -39,14 +47,4 @@ public class DataOutputStream extends FileOutputStream {
     write(value >> 8);
     write(value);
   }
-
-  public void writeBoolean(boolean value) throws IOException {
-    if (value) {
-      write(1);
-    }
-    else {
-      write(0);
-    }
-  }
-
 }
