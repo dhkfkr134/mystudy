d45ff9a2638e0a3917e32e742c3c79039287a005
dhkfkr134 <dhkfkr134@naver.com>
2024-01-23 14:44:16 +0900

    remote linux-server test

--- java-basic/app/src/main/java/com/eomcs/concurrent/ex1/Exam0110.java
+++ java-basic/app/src/main/java/com/eomcs/concurrent/ex1/Exam0110.java
@@ -12,12 +12,11 @@ public class Exam0110 {
     }
 
     for (int i = 0; i < 1000; i++) {
-      System.out.println(">>> " + i);
+      System.out.println(">>>>>>>>> " + i);
     }
   }
 }
 
 // 자바는 main() 메서드를 실행하는 한 개의 "실행 흐름"이 있다.
 // 실행 흐름에 따라 순서대로 코드가 실행된다.
-
 
