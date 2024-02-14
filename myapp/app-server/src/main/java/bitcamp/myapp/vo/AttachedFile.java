05baca4eb9e090520b3e297719f1655924800b95
dhkfkr134 <dhkfkr134@naver.com>
2024-02-14 15:07:10 +0900

    55 :prepare dao, handler, vo for using foreign key

---
+++ myapp/app-server/src/main/java/bitcamp/myapp/vo/AttachedFile.java
@@ -0,0 +1,32 @@
+package bitcamp.myapp.vo;
+
+public class AttachedFile {
+  private int no;
+  private String filepath;
+  private int boardNo;
+
+
+  public int getNo() {
+    return no;
+  }
+
+  public void setNo(int no) {
+    this.no = no;
+  }
+
+  public String getFilepath() {
+    return filepath;
+  }
+
+  public void setFilepath(String filepath) {
+    this.filepath = filepath;
+  }
+
+  public int getBoardNo() {
+    return boardNo;
+  }
+
+  public void setBoardNo(int boardNo) {
+    this.boardNo = boardNo;
+  }
+}
--- myapp/app-server/src/main/java/bitcamp/myapp/vo/Board.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/vo/Board.java
@@ -2,6 +2,7 @@ package bitcamp.myapp.vo;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.List;
 
 public class Board implements Serializable {
 
@@ -12,6 +13,7 @@ public class Board implements Serializable {
   private String content;
   private String writer;
   private Date createdDate;
+  private List<AttachedFile> files;
   
   @Override
   public String toString() {
@@ -62,5 +64,13 @@ public class Board implements Serializable {
 
   public void setCreatedDate(Date createdDate) {
     this.createdDate = createdDate;
+  }
+
+  public List<AttachedFile> getFiles() {
+    return files;
+  }
+
+  public void setFiles(List<AttachedFile> files) {
+    this.files = files;
   }
 }
