d55fcd470610f4030adaf4ba057f30dd8a45e419
dhkfkr134 <dhkfkr134@naver.com>
2024-02-16 18:03:25 +0900

    test

--- myapp/app/src/main/java/bitcamp/myapp/servlet/board/BoardDeleteServlet.java
+++ myapp/app/src/main/java/bitcamp/myapp/servlet/board/BoardDeleteServlet.java
@@ -70,10 +70,10 @@ public class BoardDeleteServlet extends HttpServlet {
 
       attachedFileDao.deleteAll(no);
       boardDao.delete(no);
-//
-//      out.println("<script>");
-//      out.println("  location.href = '/board/list'");
-//      out.println("</script>");s
+
+      out.println("<script>");
+      out.println("  location.href = '/board/list'");
+      out.println("</script>");
 
     } catch (Exception e) {
       out.println("<p>삭제 오류!</p>");
