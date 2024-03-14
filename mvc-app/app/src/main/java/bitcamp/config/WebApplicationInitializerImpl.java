d14753a896baace1e0739ec29731b9bf3c29a23e
dhkfkr134 <dhkfkr134@naver.com>
2024-03-14 15:53:44 +0900

    AbstractContextLoaderInitializer 상속하기

--- mvc-app/app/src/main/java/bitcamp/config/WebApplicationInitializerImpl.java
+++ mvc-app/app/src/main/java/bitcamp/config/WebApplicationInitializerImpl.java
@@ -5,38 +5,44 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration.Dynamic;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.checkerframework.checker.units.qual.A;
-import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.AbstractContextLoaderInitializer;
+import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
-public class WebApplicationInitializerImpl implements WebApplicationInitializer {
+public class WebApplicationInitializerImpl extends AbstractContextLoaderInitializer {
 
   private static Log log = LogFactory.getLog(WebApplicationInitializerImpl.class);
+  AnnotationConfigWebApplicationContext rootContext;
+  @Override
+  protected WebApplicationContext createRootApplicationContext() {
+    rootContext = new AnnotationConfigWebApplicationContext();
+    rootContext.register(RootConfig.class);
+    rootContext.refresh();
+    return rootContext;
+  }
+
+
 
   @Override
   public void onStartup(ServletContext servletContext) throws ServletException {
     log.debug("onStartup() 호출됨!");
 
-    AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
-    rootContext.register(RootConfig.class);
-    rootContext.refresh();
-    ContextLoaderListener listener = new ContextLoaderListener(rootContext);
-    servletContext.addListener(listener);
+    super.onStartup(servletContext);
 
     AnnotationConfigWebApplicationContext appContext = new AnnotationConfigWebApplicationContext();
-    appContext.setParent(rootContext);
+
     appContext.register(AppConfig.class);
+    appContext.setParent(rootContext);
     appContext.refresh();
     Dynamic appServletRegistration = servletContext.addServlet("app", new DispatcherServlet(appContext));
     appServletRegistration.addMapping("/app/*");
     appServletRegistration.setLoadOnStartup(1);
 
     AnnotationConfigWebApplicationContext adminContext = new AnnotationConfigWebApplicationContext();
-    appContext.setParent(rootContext);
-    appContext.register(AdminConfig.class);
-    appContext.refresh();
+    adminContext.setParent(rootContext);
+    adminContext.register(AdminConfig.class);
+    adminContext.refresh();
     Dynamic adminServletRegistration = servletContext.addServlet("admin", new DispatcherServlet(adminContext));
     adminServletRegistration.addMapping("/admin/*");
     adminServletRegistration.setLoadOnStartup(1);
